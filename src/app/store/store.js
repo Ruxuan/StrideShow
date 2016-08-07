06cdd0cdc6e31d57ad4220a6fd47be8977170e7d
Ruxuan Li <ruxuan.li@gmail.com>
2016-08-07 13:40:06 -0400

    Updated store to handle new network dispatches and added middleware

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -1,25 +1,45 @@
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
+import { socketInit } from '../actions/networkActions/socketActions';
+import { internetInit } from '../actions/networkActions/internetActions';
+import { mobileInit } from '../actions/networkActions/mobileActions';
+import { logger } from './middleware';
+import thunk from 'redux-thunk';
 
 // Reducers
 import rootReducer from '../reducers/index';
 
-// Create Store
-const defaultState = {
-  projects : window.strideshow.__INITIAL_STATE__,
-  uiState:   window.strideshow.__UI_STATE__
-};
+// Get state passed from server
+const defaultState = window.strideshow.__INITIAL_STATE__;
 
+// Dev tool enhancers
 const enhancers = compose(
+  applyMiddleware(/* logger, */ thunk),
   window.devToolsExtension ? window.devToolsExtension() : f => f
 );
 
+// Create store
 const store = createStore(
   rootReducer,
   defaultState,
   enhancers
 );
 
+// Setup network actions
+store.dispatch(socketInit());
+store.dispatch(internetInit());
+store.dispatch(mobileInit());
+
+// Browser history
 export const history = syncHistoryWithStore(browserHistory, store);
+
+// Redux hot loading
+if (module.hot) {
+  module.hot.accept('../reducers', () => {
+    const nextRootReducer = require('../reducers/index').default;
+    store.replaceReducer(nextRootReducer);
+  })
+}
+
 export default store;
